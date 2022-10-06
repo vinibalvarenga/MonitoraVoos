5d7a303eea7d46c69a0aa9687d21eed9c83596dc
unknown <11257564>
2022-10-06 16:30:26 -0300

    models do banco de dados

---
+++ rota/models.py
@@ -0,0 +1,38 @@
+from django.db import models
+import datetime
+# Create your models here.
+class Rota(models.Model):
+    id = models.IntegerField(primary_key=True)
+    codigo = models.CharField(max_length=15, null = False, unique = True)
+    aeroporto_partida = models.CharField(max_length=200, null=False)
+    aeroporto_chegada = models.CharField(max_length=200, null=False)
+    hora_partida_prevista= models.DateTimeField(auto_now=False)
+    hora_chegada_prevista= models.DateTimeField(auto_now=False)
+    origem = models.CharField(max_length=20, null=False)
+    destino = models.CharField(max_length=20, null=False)
+    aeronave = models.CharField(max_length=20, null=False)
+    companhia_aerea = models.CharField(max_length=20, null=False)
+    class Meta:
+        db_table = 'rotas'
+
+
+
+class Voo(models.Model):
+    STATUS_POSSIVEIS = (
+        ('em', 'embarcando'),
+        ('ca', 'cancelado'),
+        ('pr', 'programado'),
+        ('ta', 'taxiando'),
+        ('pt', 'pronto'),
+        ('ao', 'autorizado'),
+        ('vo', 'em_voo'),
+        ('at', 'aterrisao')
+    )
+    id = models.IntegerField(primary_key=True)
+    status = models.CharField(max_length=2, null=False, choiches=STATUS_POSSIVEIS)
+    piloto = models.CharField(max_length=20, null=False)
+    hora_partida = models.DateTimeField(auto_now=False)
+    hora_chegada = models.DateTimeField(auto_now=False)
+    data = models.DateTimeField(null=True)
+    class Meta:
+        db_table = 'voos'
