ca51bb94d09c7d0178f88b8eb866d7e35689092d
unknown <11257564>
2022-10-13 17:33:29 -0300

    Adicionando modificações da aula 7

--- MonitoraVoos/urls.py
+++ MonitoraVoos/urls.py
@@ -13,9 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path
+from rota import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('login/', views.login),
+    path('areaLogada/', views.areaLogada),
+    path('crud/', views.crud),
+    path('geracaoRelatorios/', views.geracaoRelatorios),
+    path('monitoramento/', views.monitoramento)
 ]
--- rota/views.py
+++ rota/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
 # Create your views here.
+def login(request):
+    return render(request,"login.html")
+
+def areaLogada(request):
+    return render(request,"areaLogada.html")
+
+def crud(request):
+    return render(request,"crud.html")
+
+def geracaoRelatorios(request):
+    return render(request,"geracaoRelatorios.html")
+
+def monitoramento(request):
+    return render(request,"monitoramento.html")
