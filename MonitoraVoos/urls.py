987a1a38c05f9d45ca2a74e4919801312c221620
yurialvizi <yurialvizi@gmail.com>
2022-11-05 19:50:05 -0300

    CRUD de rotas

--- MonitoraVoos/urls.py
+++ MonitoraVoos/urls.py
@@ -21,8 +21,15 @@ from django.views.generic.base import TemplateView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("django.contrib.auth.urls")),
-    path('', TemplateView.as_view(template_name='areaLogada.html'), name='home'), 
+    path('', TemplateView.as_view(template_name='areaLogada.html'), name='home'),
     path('crud/', TemplateView.as_view(template_name='crud.html'), name='crud'),
+    path('crud/criar-rota', views.RotaCreate.as_view(), name='criar-rota'),
+    path('crud/consultar-rotas', views.RotaListView.as_view(), name='consultar-rota'),
+    path('crud/consultar-rotas/<int:pk>', views.RotaDetailView.as_view(), name='rota-detail'),
+    path('crud/atualizar-rota', views.RotaUpdateListView.as_view(), name='atualizar-rota'),
+    path('crud/atualizar-rota/<int:pk>', views.RotaUpdate.as_view(), name='rota-update-form'),
+    path('crud/excluir-rota', views.RotaDeleteListView.as_view(), name='excluir-rota'),
+    path('crud/excluir-rota/<pk>', views.RotaDelete.as_view(), name='confirmar-excluir-rota'),
     path('geracaoRelatorios/', views.geracaoRelatorios),
     path('monitoramento/', views.monitoramento)
 ]
--- rota/views.py
+++ rota/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic import ListView, DetailView
+from django.urls import reverse_lazy
+# from django.contrib.auth.decorators import login_required, permission_required
+
+# from rota.forms import CriarRotaForm
+from rota.models import Rota
 
 # Create your views here.
 def login(request):
@@ -15,3 +22,44 @@ def geracaoRelatorios(request):
 
 def monitoramento(request):
     return render(request,"monitoramento.html")
+
+def get_rotas(request):
+    return render(request,"crud/consultar-rotas.html")
+
+class RotaCreate(CreateView):
+    model = Rota
+    fields = '__all__'
+    success_url = reverse_lazy('crud')
+
+class RotaUpdateListView(ListView):
+    model = Rota
+    template_name = 'crud/atualizar_rota.html'
+
+class RotaUpdate(UpdateView):
+    model = Rota
+    fields = ['aeronave', 'hora_partida_prevista', 'hora_chegada_prevista']
+    success_url = reverse_lazy('crud')
+
+class RotaListView(ListView):
+    model = Rota
+
+class RotaDetailView(DetailView):
+    model = Rota
+    template_name = 'crud/consultar-rotas/rota_detail.html'
+
+class RotaDeleteListView(ListView):
+    model = Rota
+    template_name = 'crud/excluir_rota.html'
+
+class RotaDelete(DeleteView):
+    model = Rota
+    template_name = 'crud/excluir_rota/confirmar_excluir_rota.html'
+    success_url = reverse_lazy('crud')
+
+
+
+def buscar_rota(request):
+    if request.method == "POST":
+        searched = request.POST['searched']
+        rota = Rota.objects.filter(name__contains==searched)
+    return render(request, '')
